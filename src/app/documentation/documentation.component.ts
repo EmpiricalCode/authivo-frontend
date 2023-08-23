91d6b54b871e7c6c4d95753191d9e81ed58fafad
empiricalcode <daniellongschool@gmail.com>
2023-08-23 13:31:03 -0400

    Added the rest of the documentation

--- src/app/documentation/documentation.component.ts
+++ src/app/documentation/documentation.component.ts
@@ -13,6 +13,7 @@ export class DocumentationComponent {
         title: "Authentication",
         id: "authentication",
         type: "REDIRECT",
+        access: "PUBLIC",
         url: "https://authivo.vercel.app/auth",
         description: "Redirect to this endpoint with the neccessary URL query parameters in order to use Authivo authentication.",
         request: [
@@ -44,6 +45,7 @@ export class DocumentationComponent {
         title: "Token",
         id: "token",
         type: "POST",
+        access: "PUBLIC",
         url: "https://authivo-api.vercel.app/authentication/token",
         description: "The token endpoint takes in an authentication code (among other parameters), and returns a JSON web token.",
         request: [
@@ -71,7 +73,131 @@ export class DocumentationComponent {
           },
           {
             name: "token",
-            description: "The authentication code that should be used to fetch a token."
+            description: "A JSON Web Token corresponding to your application."
+          }
+        ]
+      },
+      {
+        title: "Token Info",
+        id: "token_info",
+        type: "POST",
+        access: "PUBLIC",
+        url: "https://authivo-api.vercel.app/authorization/tokenInfo",
+        description: "The token info endpoint takes in a token, and returns information about that token.",
+        request: [
+          {
+            name: "token",
+            description: "A JSON Web Token obtained from Authivo."
+          }
+        ],
+        return: [
+          {
+            name: "status",
+            description: "The HTTP status of the response."
+          },
+          {
+            name: "response",
+            description: "The response to the request."
+          },
+          {
+            name: "valid",
+            description: "Whether or not the token is valid."
+          },
+          {
+            name: "decoded",
+            description: "The decoded body of the JSON Web Token. This field will only be present if the token is valid."
+          }
+        ]
+      },
+      {
+        title: "Get Applications",
+        id: "get_applications",
+        type: "POST",
+        access: "HOST ONLY",
+        url: "https://authivo-api.vercel.app/applications/getApplications",
+        description: "This endpoint takes in a token, and returns the applications associated with the owner of that token.",
+        request: [
+          {
+            name: "token",
+            description: "A JSON Web Token obtained from Authivo."
+          }
+        ],
+        return: [
+          {
+            name: "status",
+            description: "The HTTP status of the response."
+          },
+          {
+            name: "response",
+            description: "The response to the request."
+          },
+          {
+            name: "applications",
+            description: "A list of the user's applications."
+          }
+        ]
+      },
+      {
+        title: "Get Application By ID",
+        id: "get_application_by_id",
+        type: "POST",
+        access: "HOST ONLY",
+        url: "https://authivo-api.vercel.app/applications/getApplicationByID",
+        description: "This endpoint takes in a token, and returns the application associated with a client ID (if it exists).",
+        request: [
+          {
+            name: "token",
+            description: "A JSON Web Token obtained from Authivo."
+          },
+          {
+            name: "client_id",
+            description: "The client ID of the target application."
+          }
+        ],
+        return: [
+          {
+            name: "status",
+            description: "The HTTP status of the response."
+          },
+          {
+            name: "response",
+            description: "The response to the request."
+          },
+          {
+            name: "application",
+            description: "The target application."
+          }
+        ]
+      },
+      {
+        title: "User Data",
+        id: "user_data",
+        type: "POST",
+        access: "PUBLIC",
+        url: "https://authivo-api.vercel.app/users/userData",
+        description: "The user data endpoint takes in either a user ID or username, and returns data related to that user. Only one (either ID or username) is neccessary.",
+        request: [
+          {
+            name: "id",
+            description: "A user ID."
+          },
+          {
+            name: "username",
+            description: "A username."
+          }
+        ],
+        return: [
+          {
+            name: "status",
+            description: "The HTTP status of the response."
+          },
+          {
+            name: "response",
+            description: "The response to the request."
+          },
+          {
+            name: "data",
+            description: "The desired user data."
           }
         ]
       }
@@ -81,6 +207,21 @@ export class DocumentationComponent {
         title: "Authentication API",
         id: "authentication_api",
         indexGrouping: [0, 1]
+      },
+      {
+        title: "Authorization API",
+        id: "authorization_api",
+        indexGrouping: [2]
+      },
+      {
+        title: "Application API",
+        id: "application_api",
+        indexGrouping: [3, 4]
+      },
+      {
+        title: "Users API",
+        id: "users_api",
+        indexGrouping: [5]
       }
     ]
   }
